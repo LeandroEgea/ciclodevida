b0c3eaed1bcb104d93228fe97db2436bd874f4ed
Leandro <leonicoegea@gmail.com>
2020-04-02 19:53:22 -0300

    ciclodevida starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+ 
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
